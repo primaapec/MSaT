45b75921943966e7fa675dfad2b569bb2e93ba9b
primaapec <primaapec@gmail.com>
2018-10-07 22:17:11 +0300

    initial commit. creating empty classes

---
+++ MSaT_lb1/src/com/msitprimaapec/agents/booksTrading/BookBuyerAgent.java
@@ -0,0 +1,7 @@
+package com.msitprimaapec.agents.booksTrading;
+
+import jade.core.Agent;
+
+public class BookBuyerAgent extends Agent {
+
+}
---
+++ MSaT_lb1/src/com/msitprimaapec/agents/booksTrading/BookSellerAgent.java
@@ -0,0 +1,4 @@
+package com.msitprimaapec.agents.booksTrading;
+
+public class BookSellerAgent {
+}

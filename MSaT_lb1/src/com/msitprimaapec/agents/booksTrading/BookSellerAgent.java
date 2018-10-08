50e0760ede51ed4d6e2c6c644f06de231a0cb187
primaapec <primaapec@gmail.com>
2018-10-09 01:56:21 +0300

    Added code from tutorial. Added PurchaseOrdersServer class

--- MSaT_lb1/src/com/msitprimaapec/agents/booksTrading/BookSellerAgent.java
+++ MSaT_lb1/src/com/msitprimaapec/agents/booksTrading/BookSellerAgent.java
@@ -1,4 +1,114 @@
 package com.msitprimaapec.agents.booksTrading;
 
-public class BookSellerAgent {
+import jade.core.Agent;
+import jade.core.behaviours.*;
+import jade.lang.acl.ACLMessage;
+import jade.lang.acl.MessageTemplate;
+
+import java.util.Hashtable;
+
+public class BookSellerAgent extends Agent {
+    // The catalogue of books for sale (maps the title of a book to its price)
+    private Hashtable catalogue;
+
+    // Put agent initializations here
+    protected void setup() {
+
+        // Create the catalogue
+        catalogue = new Hashtable();
+        Object[] args = getArguments();
+        if (args != null && args.length > 0) {
+            System.out.println("Hello!");
+            args = ((String)args[0]).split(" ");
+            for (int i = 0; i < args.length / 2; i++) {
+                catalogue.put((String) args[2 * i], Integer.valueOf((String) args[2 * i + 1]));
+            }
+        } else {
+            // Make the agent terminate
+            System.out.println("No books catalog specified");
+            doDelete();
+        }
+        // Add the behaviour serving requests for offer from buyer agents
+        addBehaviour(new OfferRequestsServer());
+        // Add the behaviour serving purchase orders from buyer agents
+        addBehaviour(new PurchaseOrdersServer());
+    }
+
+    // Put agent clean-up operations here
+    protected void takeDown() {
+        // Printout a dismissal message
+        System.out.println("Seller-agent " + getAID().getName() + " terminating.");
+    }
+
+    /**
+     * This is invoked by the GUI when the user adds a new book for sale
+     */
+    public void updateCatalogue(final String title, final int price) {
+        addBehaviour(new OneShotBehaviour() {
+            public void action() {
+                catalogue.put(title, new Integer(price));
+            }
+        });
+    }
+
+
+    /**
+     * Inner class OfferRequestsServer.
+     * This is the behaviour used by Book-seller agents to serve incoming requests
+     * for offer from buyer agents.
+     * If the requested book is in the local catalogue the seller agent replies
+     * with a PROPOSE message specifying the price. Otherwise a REFUSE message is
+     * sent back.
+     */
+
+    private class OfferRequestsServer extends CyclicBehaviour {
+        public void action() {
+            ACLMessage msg = myAgent.receive(MessageTemplate.MatchPerformative(ACLMessage.CFP));
+            if (msg != null) {
+                // Message received. Process it
+                String title = msg.getContent();
+                ACLMessage reply = msg.createReply();
+                Integer price = (Integer) catalogue.get(title);
+                if (price != null) {
+                    // The requested book is available for sale. Reply with the price
+                    reply.setPerformative(ACLMessage.PROPOSE);
+                    reply.setContent(String.valueOf(price.intValue()));
+                } else {
+                    // The requested book is NOT available for sale.
+                    reply.setPerformative(ACLMessage.REFUSE);
+                    reply.setContent("not-available");
+                }
+                myAgent.send(reply);
+            }
+            else
+            {
+                block();
+            }
+        }
+    } // End of inner class OfferRequestsServer
+
+    private class PurchaseOrdersServer extends CyclicBehaviour {
+        public void action() {
+            ACLMessage msg = myAgent.receive(MessageTemplate.MatchPerformative(ACLMessage.ACCEPT_PROPOSAL));
+            if (msg != null) {
+                String title = msg.getContent();
+                ACLMessage reply = msg.createReply();
+                Integer price = (Integer) catalogue.get(title);
+                if (price != null) {
+                    // The requested book is available for sale. Reply with the confirmation
+                    reply.setPerformative(ACLMessage.INFORM);
+                    catalogue.remove(title);
+                } else {
+                    // The requested book is NOT available for sale.
+                    reply.setPerformative(ACLMessage.REFUSE);
+                    reply.setContent("not-available");
+                }
+                myAgent.send(reply);
+            }
+            else
+            {
+                block();
+            }
+        }
+    }
 }

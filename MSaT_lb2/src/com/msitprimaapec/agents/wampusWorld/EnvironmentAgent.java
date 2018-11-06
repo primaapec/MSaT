2d4a648f912a50cdc0b4e1a0dcc73b1ff6a82869
primaapec <primaapec@gmail.com>
2018-11-06 09:49:30 +0200

    Adding communication skeleton to environment agent

--- MSaT_lb2/src/com/msitprimaapec/agents/wampusWorld/EnvironmentAgent.java
+++ MSaT_lb2/src/com/msitprimaapec/agents/wampusWorld/EnvironmentAgent.java
@@ -1,7 +1,105 @@
 package com.msitprimaapec.agents.wampusWorld;
 
+import jade.core.AID;
 import jade.core.Agent;
+import jade.core.behaviours.CyclicBehaviour;
+import jade.core.behaviours.OneShotBehaviour;
+import jade.domain.DFService;
+import jade.domain.FIPAAgentManagement.DFAgentDescription;
+import jade.domain.FIPAAgentManagement.ServiceDescription;
+import jade.domain.FIPAException;
+import jade.lang.acl.ACLMessage;
 
 public class EnvironmentAgent extends Agent {
 
+    private AID spelAgent;
+
+    @Override
+    protected void setup() {
+        System.out.println("Environment agent " + getAID().getName() + " is ready!");
+        DFAgentDescription dfd = new DFAgentDescription();
+        dfd.setName(getAID());
+        ServiceDescription sd = new ServiceDescription();
+        sd.setType("Wumpus-World-Cave");
+        sd.setName("Cave-wandering");
+        dfd.addServices(sd);
+        try {
+            DFService.register(this, dfd);
+        }
+        catch (FIPAException fe) {
+            fe.printStackTrace();
+        }
+        addBehaviour(new RequestBehavior());
+    }
+
+    private class RequestBehavior extends CyclicBehaviour {
+        public void action() {
+            ACLMessage msg = myAgent.receive();
+            if (msg != null) {
+                if (spelAgent == null)
+                    spelAgent = msg.getSender();
+                if (spelAgent.equals(msg.getSender())) {
+                    if (msg.getPerformative() == ACLMessage.REQUEST)
+                        myAgent.addBehaviour(new PerceptReplyBehaviour(msg));
+                    if (msg.getPerformative() == ACLMessage.CFP)
+                        myAgent.addBehaviour(new WorldChangingBehaviour(msg));
+                }
+                else {
+                    ACLMessage reply = msg.createReply();
+                    reply.setPerformative(ACLMessage.REFUSE);
+                    reply.setContent("not-available");
+                }
+            }
+            else
+            {
+                block();
+            }
+        }
+    }
+
+    private class PerceptReplyBehaviour extends OneShotBehaviour {
+
+        ACLMessage msg;
+
+        public PerceptReplyBehaviour(ACLMessage m)
+        {
+            super();
+            msg = m;
+        }
+
+        public void action() {
+            String content = msg.getContent();
+            ACLMessage reply = msg.createReply();
+            if (content != null) {
+                reply.setPerformative(ACLMessage.INFORM);
+                reply.setContent(GeneratePerceptSequence());
+            } else {
+                reply.setPerformative(ACLMessage.REFUSE);
+                reply.setContent("not-available");
+            }
+            myAgent.send(reply);
+        }
+
+        private String GeneratePerceptSequence() {
+            String reply = "";
+            // TODO generate percept string
+            return reply;
+        }
+    }
+
+    private class WorldChangingBehaviour extends OneShotBehaviour {
+
+        ACLMessage msg;
+
+        public WorldChangingBehaviour(ACLMessage m)
+        {
+            super();
+            msg = m;
+        }
+
+        public void action() {
+            String content = msg.getContent();
+            // TODO process message and change the world
+        }
+    }
 }
